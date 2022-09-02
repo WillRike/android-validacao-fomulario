488f700ef35e134191f3f4077e2a5684bf62302e
Willen Rike <38054943+WillRike@users.noreply.github.com>
2022-09-02 14:54:25 -0300

    Criando formulario

---
+++ app/src/main/java/alura/com/br/alurafood/ui/FormularioCadastroActivity.java
@@ -0,0 +1,16 @@
+package alura.com.br.alurafood.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import alura.com.br.alurafood.R;
+
+public class FormularioCadastroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_cadastro);
+    }
+}
